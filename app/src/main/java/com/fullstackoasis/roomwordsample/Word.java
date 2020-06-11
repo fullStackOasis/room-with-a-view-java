bef389b0c2c4d94ca68b8ba4ff1a580e4a1e6a06
fullStackOasis <fullstackdev@fullstackoasis.com>
2020-06-11 11:41:43 -0400

    Create the Word Entity https://codelabs.developers.google.com/codelabs/android-room-with-a-view/#4

---
+++ app/src/main/java/com/fullstackoasis/roomwordsample/Word.java
@@ -0,0 +1,13 @@
+package com.fullstackoasis.roomwordsample;
+
+import androidx.annotation.NonNull;
+
+public class Word {
+
+    private String mWord;
+
+    public Word(@NonNull String word) {this.mWord = word;}
+
+    public String getWord(){return this.mWord;}
+}
+
